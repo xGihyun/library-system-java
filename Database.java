a9cdf462d99daa0e48f3dac911698c76a0d9fdcc
xGihyun <giordnnuz@gmail.com>
2024-05-07 12:41:34 +0800

    Add example select query

--- Database.java
+++ Database.java
@@ -4,18 +4,42 @@ public class Database {
 
   public static void main(String[] args) {
     Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    System.out.println("Connecting to database...");
 
     try {
+      // Load the JDBC driver
+      Class.forName("org.mariadb.jdbc.Driver");
 
-      String dbName = "";
-      String userName = "";
+      String dbName = "enrollment"; // Change this
+      String username = "root";
       String password = "";
-      String connectionUrl = "";
+      String url = "jdbc:mariadb://localhost/" + dbName;
 
-      conn = DriverManager.getConnection(connectionUrl);
+      // Connect to the database
+      conn = DriverManager.getConnection(url, username, password);
 
+      // Create a statement object
+      stmt = conn.createStatement();
+
+      // Define the select * query
+      String sql = "SELECT * FROM users";
+
+      // Execute the query and get the results
+      rs = stmt.executeQuery(sql);
+
+      // Process the results (assuming you have multiple columns)
+      while (rs.next()) {
+        int id = rs.getInt("id"); // Get the value of the "id" column
+
+        System.out.println("ID: " + id);
+      }
+
+      System.out.println("Connected to database.");
     } catch (Exception e) {
-      // TODO: handle exception
+      throw new IllegalStateException("Cannot connect the database!", e);
     }
 
   }
